84fca444c8828b6612207f151f81069358d5830c
chao.li <lichao7@chinasie.com>
2022-08-26 17:56:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Introduction
+基于Spring Cloud Gateway的微服务网关服务
+
+## Features
+
+- 用户鉴权
+- 限流
+- 整体运维
+- API监控
+
+## Contributing
+
+欢迎参与项目贡献！比如提交PR修复一个bug，或者新建Issue讨论新特性或者变更。
+
+Copyright (c) 2020-present, HETU
---
+++ src/test/java/com/hetu/gateway/HetuGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hetu.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HetuGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
